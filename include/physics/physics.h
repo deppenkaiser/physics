f93f2a96615c5c4808ebde99ee34251806aa1bd8
Dipl.-Ing. (FH) Michael Czybor <contact@log1cworx.at>
2024-03-12 15:00:47 +0100

    ADD functions

--- include/physics/physics.h
+++ include/physics/physics.h
@@ -2,6 +2,7 @@
 
 #include <vector/vector.h>
 
+#define PHYSICS_SECONDS_PER_DAY (24.0 * 3600.0)
 #define PHYSICS_C 299792458.0
 #define PHYSICS_C_SQUARE (299792458.0 * 299792458.0)
 #define PHYSICS_SUN_MASS 1.9884e30
@@ -9,5 +10,11 @@
 #define PHYSICS_VENUS_MASS 4.8673e24
 #define PHYSICS_EARTH_MASS 5.9722e24
 #define PHYSICS_MOON_MASS 7.346e22
+#define PHYSICS_MOON_ANGULAR_SPEED (2 * physics_pi() / (PHYSICS_SECONDS_PER_DAY * 27.322))
+#define PHYSICS_EARTH_MOON_DISTANCE 384400.0e3
 
+double physics_pi();
 struct vector_3d physics_weber_gravitation_force(double m1_kg, double m2_kg, vector_3d_t r, vector_3d_t v, vector_3d_t a);
+struct vector_3d physics_tangential_speed(vector_3d_t w, vector_3d_t r);
+struct vector_3d physics_linear_motion(vector_3d_t v, double t);
+double physics_barycenter(double distance_m, double mass_center_kg, double mass_satellite_kg);
--- src/physics.c
+++ src/physics.c
@@ -1,7 +1,29 @@
 #include "physics/physics.h"
+#include <vector/vector.h>
+#include <math.h>
+
+double physics_pi()
+{
+    return acos(-1.0);
+}
+
+double physics_barycenter(double distance_m, double mass_center_kg, double mass_satellite_kg)
+{
+    return distance_m * mass_satellite_kg / (mass_center_kg + mass_satellite_kg);
+}
 
 struct vector_3d physics_weber_gravitation_force(double m1_kg, double m2_kg, vector_3d_t r, vector_3d_t v, vector_3d_t a)
 {
     struct vector_3d F = {0};
     return F;
+}
+
+struct vector_3d physics_tangential_speed(vector_3d_t w, vector_3d_t r)
+{
+    return vector_cross(w, r);
+}
+
+struct vector_3d physics_linear_motion(vector_3d_t v, double t)
+{
+    return vector_multiply_scalar(v, t);
 }
