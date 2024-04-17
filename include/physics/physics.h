d6d7abc4fc645bbb2d0c73ed24dfcfb5c01aef92
Dipl.-Ing. (FH) Michael Czybor <contact@log1cworx.at>
2024-04-17 12:43:00 +0200

    new functions

--- include/physics/physics.h
+++ include/physics/physics.h
@@ -26,16 +26,16 @@ typedef enum
 
 void physics_initialize();
 const char* physics_body_name(physics_body_id_t id);
-double physics_body_mass(physics_body_id_t id);
+double physics_body_mass_kg(physics_body_id_t id);
+double physics_body_p_AU(physics_body_id_t id);
+double physics_body_eccentricity(physics_body_id_t id);
 double physics_body_perihel_AU(physics_body_id_t id);
 double physics_body_aphel_AU(physics_body_id_t id);
-double physics_initial_radii_AU(physics_body_id_t id);
 double physics_pi();
 double physics_seconds_per_day();
-double physics_seconds_per_siderial_day();
 double physics_barycenter_AU(double distance_AU, double mass_center_kg, double mass_satellite_kg);
-struct vector_3d physics_newton_angular_speed(vector_3d_t r_AU, double center_mass_kg);
 struct vector_3d physics_weber_gravitation_force(double m1_kg, double m2_kg, vector_3d_t r, vector_3d_t v, vector_3d_t a);
 struct vector_3d physics_tangential_speed(vector_3d_t w, vector_3d_t r);
 struct vector_3d physics_linear_motion(vector_3d_t v, double t_s);
 struct vector_3d physics_linear_angle(vector_3d_t w, double t_s);
+struct vector_3d physics_kepler_position_AU(physics_body_id_t id, double angle_rad);
--- src/physics.c
+++ src/physics.c
@@ -14,42 +14,65 @@
 #define PHYSICS_URANUS_MASS 8.681e25
 #define PHYSICS_NEPTUNE_MASS 1.024e26
 
-double _physics_perihels[] =
-{
-    0.0, // sun
-    0.0, // sun_system_barycenter
-    0.308, // mercury
-    0.718, // venus
-    0.0, // earth
-    363300.0e3 / PHYSICS_AU, // moon
-    0.983, // earth-moon-barycenter
-    1.382,
-    4.95,
-    9.063,
-    18.295,
-    29.707
-};
+#define PHYSICS_SUN_P 0.0
+#define PHYSICS_MERCURY_P 0.3876
+#define PHYSICS_VENUS_P 0.7233
+#define PHYSICS_EARTH_P 1.0
+#define PHYSICS_MOON_P (384400.0e3 / PHYSICS_AU)
+#define PHYSICS_MARS_P 1.524
+#define PHYSICS_JUPITER_P 5.204
+#define PHYSICS_SATURN_P 9.582
+#define PHYSICS_URANUS_P 19.201
+#define PHYSICS_NEPTUNE_P 30.047
 
-double _physics_aphels[] =
-{
-    0.0, // sun
-    0.0, // sun_system_barycenter
-    0.467, // mercury
-    0.728, // venus
-    0.0, // earth
-    405500.0e3 / PHYSICS_AU, // moon
-    1.017, // earth-moon-barycenter
-    1.666,
-    5.458,
-    10.101,
-    20.107,
-    30.387
-};
+#define PHYSICS_SUN_ECCENTRICITY 0.0
+#define PHYSICS_MERCURY_ECCENTRICITY 0.2056
+#define PHYSICS_VENUS_ECCENTRICITY 0.0067
+#define PHYSICS_EARTH_ECCENTRICITY 0.0167
+#define PHYSICS_MOON_ECCENTRICITY 0.0549
+#define PHYSICS_MARS_ECCENTRICITY 0.0934
+#define PHYSICS_JUPITER_ECCENTRICITY 0.0489
+#define PHYSICS_SATURN_ECCENTRICITY 0.0542
+#define PHYSICS_URANUS_ECCENTRICITY 0.0472
+#define PHYSICS_NEPTUNE_ECCENTRICITY 0.0113
 
-double _physics_body_masses[] =
+double _physics_body_p_AU[] =
+    {
+        PHYSICS_SUN_P,
+        0,
+        PHYSICS_MERCURY_P,
+        PHYSICS_VENUS_P,
+        PHYSICS_EARTH_P,
+        PHYSICS_MOON_P,
+        0,
+        PHYSICS_MARS_P,
+        PHYSICS_JUPITER_P,
+        PHYSICS_SATURN_P,
+        PHYSICS_URANUS_P,
+        PHYSICS_NEPTUNE_P
+    };
+
+double _physics_body_eccentricity[] =
+    {
+        PHYSICS_SUN_ECCENTRICITY,
+        0,
+        PHYSICS_MERCURY_ECCENTRICITY,
+        PHYSICS_VENUS_ECCENTRICITY,
+        PHYSICS_EARTH_ECCENTRICITY,
+        PHYSICS_MOON_ECCENTRICITY,
+        0,
+        PHYSICS_MARS_ECCENTRICITY,
+        PHYSICS_JUPITER_ECCENTRICITY,
+        PHYSICS_SATURN_ECCENTRICITY,
+        PHYSICS_URANUS_ECCENTRICITY,
+        PHYSICS_NEPTUNE_ECCENTRICITY
+    };
+
+double _physics_body_masses_kg[] =
     {
         PHYSICS_SUN_MASS,
-        PHYSICS_SUN_MASS + PHYSICS_MERCURY_MASS + PHYSICS_VENUS_MASS + PHYSICS_EARTH_MASS + PHYSICS_MOON_MASS + PHYSICS_MARS_MASS,
+        PHYSICS_SUN_MASS + PHYSICS_MERCURY_MASS + PHYSICS_VENUS_MASS + PHYSICS_EARTH_MASS + PHYSICS_MOON_MASS +
+            PHYSICS_MARS_MASS + PHYSICS_JUPITER_MASS + PHYSICS_SATURN_MASS + PHYSICS_URANUS_MASS + PHYSICS_NEPTUNE_MASS,
         PHYSICS_MERCURY_MASS,
         PHYSICS_VENUS_MASS,
         PHYSICS_EARTH_MASS,
@@ -78,56 +101,18 @@ const char* _physics_body_names[] =
         "neptune"
     };
 
-double _physics_initial_radii_AU[] =
-    {
-        0.0, // sun
-        0.0, // sun_system_barycenter
-        0.0,
-        0.0,
-        0.0, // earth
-        0.0, // moon
-        0.0, // earth-moon-barycenter
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-        0.0
-    };
-
 void physics_initialize()
 {
-    for (uint32_t i = 0; i < PHYSICS_BODY_COUNT; ++i)
-    {
-        switch (i)
-        {
-            case EARTH:
-                _physics_initial_radii_AU[i] = physics_barycenter_AU(physics_body_aphel_AU(MOON), physics_body_mass(EARTH), physics_body_mass(MOON));
-                break;
-
-            case MOON:
-                _physics_initial_radii_AU[i] = -(physics_body_aphel_AU(MOON) - _physics_initial_radii_AU[EARTH]);
-                break;
-
-            default:
-                _physics_initial_radii_AU[i] = physics_body_aphel_AU(i);
-                break;
-        }
-    }
 }
 
 double physics_body_perihel_AU(physics_body_id_t id)
 {
-    return _physics_perihels[id];
+    return 0.0;
 }
 
 double physics_body_aphel_AU(physics_body_id_t id)
 {
-    return _physics_aphels[id];
-}
-
-double physics_initial_radii_AU(physics_body_id_t id)
-{
-    return _physics_initial_radii_AU[id];
+    return 0.0;
 }
 
 const char* physics_body_name(physics_body_id_t id)
@@ -135,9 +120,19 @@ const char* physics_body_name(physics_body_id_t id)
     return _physics_body_names[id];
 }
 
-double physics_body_mass(physics_body_id_t id)
+double physics_body_mass_kg(physics_body_id_t id)
 {
-    return _physics_body_masses[id];
+    return _physics_body_masses_kg[id];
+}
+
+double physics_body_p_AU(physics_body_id_t id)
+{
+    return _physics_body_p_AU[id];
+}
+
+double physics_body_eccentricity(physics_body_id_t id)
+{
+    return _physics_body_eccentricity[id];
 }
 
 double physics_pi()
@@ -150,11 +145,6 @@ double physics_seconds_per_day()
     return 24.0 * 3600.0;
 }
 
-double physics_seconds_per_siderial_day()
-{
-    return 86164.0989;
-}
-
 double physics_barycenter_AU(double distance_AU, double mass_center_kg, double mass_satellite_kg)
 {
     return distance_AU * mass_satellite_kg / (mass_center_kg + mass_satellite_kg);
@@ -162,7 +152,7 @@ double physics_barycenter_AU(double distance_AU, double mass_center_kg, double m
 
 struct vector_3d physics_newton_angular_speed(vector_3d_t r_AU, double center_mass_kg)
 {
-    struct vector_3d w = {0.0, 0.0, sqrt(center_mass_kg * PHYSICS_G / pow(vector_norm(r_AU) * PHYSICS_AU, 3.0))};
+    struct vector_3d w = {0};
     return w;
 }
 
@@ -185,4 +175,11 @@ struct vector_3d physics_linear_motion(vector_3d_t v, double t_s)
 struct vector_3d physics_linear_angle(vector_3d_t w, double t_s)
 {
     return vector_multiply_scalar(w, t_s);
+}
+
+struct vector_3d physics_kepler_position_AU(physics_body_id_t id, double angle_rad)
+{
+    double R_AU = physics_body_p_AU(id) / (physics_body_eccentricity(id) * cos(angle_rad) + 1.0);
+    struct vector_3d r_AU = {cos(angle_rad) * R_AU, sin(angle_rad) * R_AU, 0.0};
+    return r_AU;
 }
