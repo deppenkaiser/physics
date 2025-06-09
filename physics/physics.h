61519101ad3c1f28e811fdfa6f77a301f05b9088
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2025-06-09 12:06:07 +0200

    Anderes Modell

--- physics/physics.h
+++ physics/physics.h
@@ -10,34 +10,87 @@
 #define PHYSICS_CALCIUM_FILTER_WAVELENGTH_M 393.3e-9
 #define PHYSICS_GREEN_FILTER_M 540.0e-9
 
-typedef enum
-{
-    SUN = 0,
-    SUN_BARYCENTER = 1,
-    MERCURY = 2,
-    VENUS = 3,
-    EARTH = 4,
-    MOON = 5,
-    EARTH_MOON_BARYCENTER = 6,
-    MARS = 7,
-    JUPITER = 8,
-    SATURN = 9,
-    URANUS = 10,
-    NEPTUNE = 11,
-    PHYSICS_BODY_COUNT = 12
-} physics_body_id_t;
+// https://www.imcce.fr/
 
-const char* physics_body_name(physics_body_id_t id);
-double physics_body_mass_kg(physics_body_id_t id);
-double physics_body_a_AU(physics_body_id_t id);
-double physics_body_eccentricity(physics_body_id_t id);
-double physics_body_i_rad(physics_body_id_t id);
-double physics_body_node_rad(physics_body_id_t id);
-double physics_body_w_rad(physics_body_id_t id);
-double physics_body_perihel_AU(physics_body_id_t id);
-double physics_body_aphel_AU(physics_body_id_t id);
-double physics_czybor_angular_speed_rad_per_s(physics_body_id_t planet_id, physics_body_id_t center_id, double phi_rad);
-double physics_czybor_mean_angular_speed_rad_per_s(physics_body_id_t planet_id, physics_body_id_t center_id);
+#define PHYSICS_SUN_MASS 1.9884e30
+#define PHYSICS_MERCURY_MASS 3.301e23
+#define PHYSICS_VENUS_MASS 4.8673e24
+#define PHYSICS_EARTH_MASS 5.9722e24
+#define PHYSICS_MOON_MASS 7.346e22
+#define PHYSICS_MARS_MASS 6.417e23
+#define PHYSICS_JUPITER_MASS 1.89813e27
+#define PHYSICS_SATURN_MASS 5.683e26
+#define PHYSICS_URANUS_MASS 8.681e25
+#define PHYSICS_NEPTUNE_MASS 1.024e26
+
+#define PHYSICS_SUN_A 0.0
+#define PHYSICS_MERCURY_A 0.3870983098
+#define PHYSICS_VENUS_A 0.7233298200
+#define PHYSICS_EARTH_A 1.0000010178
+#define PHYSICS_MOON_A (384400.0e3 / PHYSICS_AU)
+#define PHYSICS_MARS_A 1.5236793419
+#define PHYSICS_JUPITER_A 5.2026032092
+#define PHYSICS_SATURN_A 9.5549091915
+#define PHYSICS_URANUS_A 19.2184460618
+#define PHYSICS_NEPTUNE_A 30.1103868694
+
+#define PHYSICS_SUN_ECCENTRICITY 0.0
+#define PHYSICS_MERCURY_ECCENTRICITY 0.2056317526
+#define PHYSICS_VENUS_ECCENTRICITY 0.0067719164
+#define PHYSICS_EARTH_ECCENTRICITY 0.0167086342
+#define PHYSICS_MOON_ECCENTRICITY 0.0549
+#define PHYSICS_MARS_ECCENTRICITY 0.0934006477
+#define PHYSICS_JUPITER_ECCENTRICITY 0.0484979255
+#define PHYSICS_SATURN_ECCENTRICITY 0.0555481426
+#define PHYSICS_URANUS_ECCENTRICITY 0.0463812221
+#define PHYSICS_NEPTUNE_ECCENTRICITY 0.009455747
+
+#define PHYSICS_SUN_I 0.0
+#define PHYSICS_MERCURY_I 7.00498625
+#define PHYSICS_VENUS_I 3.39466189
+#define PHYSICS_EARTH_I 0.0
+#define PHYSICS_MOON_I 0.0
+#define PHYSICS_MARS_I 1.84972648
+#define PHYSICS_JUPITER_I 1.30326698
+#define PHYSICS_SATURN_I 2.48887878
+#define PHYSICS_URANUS_I 0.77319689
+#define PHYSICS_NEPTUNE_I 1.76995259
+
+// Aufsteigender Knoten
+#define PHYSICS_SUN_NODE 0.0
+#define PHYSICS_MERCURY_NODE 48.33089304
+#define PHYSICS_VENUS_NODE 76.67992019
+#define PHYSICS_EARTH_NODE 0.0
+#define PHYSICS_MOON_NODE 0.0
+#define PHYSICS_MARS_NODE 49.55809321
+#define PHYSICS_JUPITER_NODE 100.46440702
+#define PHYSICS_SATURN_NODE 113.66550252
+#define PHYSICS_URANUS_NODE 74.00595701
+#define PHYSICS_NEPTUNE_NODE 131.78405702
+
+// Perihellänge
+#define PHYSICS_SUN_PL 0.0
+#define PHYSICS_MERCURY_PL 77.45611904
+#define PHYSICS_VENUS_PL 131.56370300
+#define PHYSICS_EARTH_PL 102.93734808
+#define PHYSICS_MOON_PL 102.93734808
+#define PHYSICS_MARS_PL 336.06023395
+#define PHYSICS_JUPITER_PL 14.33120687
+#define PHYSICS_SATURN_PL 93.05723748
+#define PHYSICS_URANUS_PL 173.00529106
+#define PHYSICS_NEPTUNE_PL 48.12027554
+
+#define PHYSICS_SUN_W 0.0
+#define PHYSICS_MERCURY_W 29.125226
+#define PHYSICS_VENUS_W 54.88378281
+#define PHYSICS_EARTH_W 100.46645683
+#define PHYSICS_MOON_W 100.46645683
+#define PHYSICS_MARS_W 286.50214074
+#define PHYSICS_JUPITER_W 273.86679985
+#define PHYSICS_SATURN_W 339.39173496
+#define PHYSICS_URANUS_W 98.99933405
+#define PHYSICS_NEPTUNE_W 276.33621852
+
 double physics_pi();
 double physics_frac(double x);
 double physics_modulo(double a, double b);
@@ -45,10 +98,7 @@ double physics_deg_to_rad();
 double physics_rad_to_deg();
 double physics_seconds_per_day();
 double physics_barycenter_AU(double distance_AU, double mass_center_kg, double mass_satellite_kg);
-double physics_orbital_period_s(physics_body_id_t planet_id, physics_body_id_t center_id, double phi_1_rad, double phi_2_rad, uint32_t step_count);
-double physics_optimized_orbital_period_s(physics_body_id_t planet_id, physics_body_id_t center_id, double phi_1_rad, double phi_2_rad);
-double physics_full_orbital_period_days(physics_body_id_t planet_id, physics_body_id_t center_id);
 double physics_rayleigh_criteria_deg(double wavelength_light_m, double objective_aperture_m);
 double physics_image_sensor_object_size_m(double object_size_deg, double focal_length_m);
 double physics_needed_image_sensor_pixel_size_m(double wavelength_light_m, double objective_aperture_m, double focal_length_m);
-struct vector_3d physics_kepler_r_AU(physics_body_id_t id, double angle_rad);
+double physics_specific_angular_momentum(double mass_center_kg, double a_m, double eccentricity);
